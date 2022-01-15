4ba396e380a0a2934811d41183690be24f9aed2b
Ricardo3113 <ricardofariasdesouza@hotmail.com>
2022-01-15 19:45:10 -0300

    Props

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -33,6 +33,8 @@ INSERT INTO tb_movie(score, count, title, image) VALUES (0, 0, 'Interestelar', '
 INSERT INTO tb_movie(score, count, title, image) VALUES (0, 0, 'Contato', 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/yFkUPqBuUnbhYbQL8VFpTrAT9za.jpg');
 INSERT INTO tb_movie(score, count, title, image) VALUES (0, 0, 'Duna', 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jYEW5xZkZk2WTrdbMGAPFuBqbDc.jpg');
 INSERT INTO tb_movie(score, count, title, image) VALUES (0, 0, 'Aquaman', 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/2cUsDz4TzFYHrKktT1bKHHQ7Cgm.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (0, 0, 'Os Nerds Saem de Férias', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/yR9FfGTOdDqjVzH8Ul8fvwPhU1f.jpg');
+INSERT INTO tb_movie(score, count, title, image) VALUES (0, 0, 'Curtindo a Vida Adoidado, 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4HambjOlwbOp5BOmHR0ZAY8ijDm.jpg');
 
 INSERT INTO tb_score(movie_id, user_id, value) VALUES (1, 1, 5.0);
 INSERT INTO tb_score(movie_id, user_id, value) VALUES (1, 2, 4.0);
